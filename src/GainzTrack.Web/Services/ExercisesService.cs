31240b50dba049b534120f59080aadbc8dfe51fa
GalinShterev <galin.shterev@outlook.com>
2018-11-19 18:28:12 +0200

    Added new services

---
+++ src/GainzTrack.Web/Services/ExercisesService.cs
@@ -0,0 +1,21 @@
+﻿using GainzTrack.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GainzTrack.Web.Services
+{
+    public class ExercisesService : IExercisesService
+    {
+        private readonly ApplicationDbContext _context;
+        public ExercisesService(ApplicationDbContext context)
+        {
+            _context = context;       
+        }
+        public IEnumerable<string> GetExercisesNames()
+        {
+            return _context.Exercises.Select(x => x.Name).ToList();
+        }
+    }
+}

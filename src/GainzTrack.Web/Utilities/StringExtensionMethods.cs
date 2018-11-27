b23a2205a4eb753f4f9c891114d1ddc2479592c3
GalinShterev <galin.shterev@outlook.com>
2018-11-27 17:05:38 +0200

    Added view component for listing exercises

---
+++ src/GainzTrack.Web/Utilities/StringExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GainzTrack.Web.Utilities
+{
+    public static class StringExtensionMethods
+    {
+        public static string DecodeUrl(this string str)
+        {
+            return WebUtility.UrlDecode(str);
+        }
+    }
+}
---
+++ src/GainzTrack.Web/ViewComponents/AddExercisesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using GainzTrack.Core.Entities;
+using GainzTrack.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GainzTrack.Web.ViewComponents
+{
+    public class AddExercisesViewComponent : ViewComponent
+    {
+        private readonly IRepository _repository;
+        public AddExercisesViewComponent(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var exercises = _repository.List<Exercise>();
+
+            return this.View(exercises);
+        }
+    }
+}
